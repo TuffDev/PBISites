7900206a70aa6613eadb0d54889d65ac96a4d783
Adam Martin <cooladam98@gmail.com>
2019-07-29 08:48:55 -0400

    update build file

---
+++ build/precache-manifest.af38484e87b96ca3a949c4c04e56ef5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95479f9a0ef1d2d26acc034f48b54788",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3976254bc9a0d87f29d",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "d32de78f8e8626cb0d5b",
+    "url": "/static/js/2.a7d2f531.chunk.js"
+  },
+  {
+    "revision": "b3976254bc9a0d87f29d",
+    "url": "/static/js/main.f19bf1d4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
